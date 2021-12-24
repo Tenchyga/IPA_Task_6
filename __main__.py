6132a7f559f3a8df5f9c588da69b3d93bceb715f
Tenchyga <tenchyga@mail.ru>
2021-12-25 01:54:12 +0700

    Создание файлов инициализации и точки входа.

---
+++ __main__.py
@@ -0,0 +1,9 @@
+from intersection import intersection
+from union import union
+
+def main():
+    print(intersection(2,3,5,1,3,4,7,2))
+    print(union(2,3,5,1,3,4,7,2))
+
+if __name__ == '__main__':
+    main()
